9c09dc0af4795daee7a242a11172d3dffc88ce4e
signeth2 <74925648+signeth2@users.noreply.github.com>
2022-09-16 13:00:11 +0200

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,35 @@
+function changeImage() {
+    var image = document.getElementById('myImage');
+    image.src = "../src/img/img5.png";
+
+}
+
+function changeImage2() {
+    var image = document.getElementById('myImage2');
+    image.src = "../src/img/img12.png";
+
+}
+
+function changeImage3() {
+    var image = document.getElementById('myImage3');
+    image.src = "../src/img/img9.png";
+
+}
+
+function changeImage4() {
+    var image = document.getElementById('myImage4');
+    image.src = "../src/img/img1.png";
+
+}
+
+function changeImage5() {
+    var image = document.getElementById('myImage5');
+    image.src = "../src/img/img6.png";
+
+}
+
+function changeImage6() {
+    var image = document.getElementById('myImage6');
+    image.src = "../src/img/img6.png";
+
+}
